1def7a6ca4d950423d35c3bc9fd6d8d490bb43a4
Erivan Buenaventura <109859566+evan-07@users.noreply.github.com>
2023-11-29 16:58:09 +0800

    Update Day 7 - Hangman4.py

--- Day 7 - Hangman4.py
+++ Day 7 - Hangman4.py
@@ -2,7 +2,8 @@
 
 import random
 
-stages = ['''
+stages = [
+    '''
   +---+
   |   |
   O   |
@@ -57,15 +58,15 @@ stages = ['''
       |
       |
 =========
-''']
+'''
+]
 
 end_of_game = False
 word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
-
-#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
+#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
 #Set 'lives' to equal 6.
 lives = 6
 #Testing code
@@ -80,26 +81,26 @@ while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
     correct_letter = 0
-  
+
     #Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
         # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
-      
+
         if letter == guess:
             display[position] = letter
             correct_letter += 1
 
     #TODO-2: - If guess is not a letter in the chosen_word,
-    #Then reduce 'lives' by 1. 
+    #Then reduce 'lives' by 1.
     if not correct_letter > 0:
-       lives -= 1
-  
+        lives -= 1
+
     #If lives goes down to 0 then the game should stop and it should print "You lose."
     if lives <= 0:
         end_of_game = True
         print("You lose.")
-       
+
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
 
@@ -109,4 +110,4 @@ while not end_of_game:
         print("You win.")
 
     #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
-    print(stage[lives])
+    print(stages[lives])
