347b4d4889ebb3126e6d18f1ed783c551269ad89
van <erivanbuenaventura@gmail.com>
2023-12-18 14:31:49 +0800

    completed coffee maker using provided classes

--- day_16/main.py
+++ day_16/main.py
@@ -1,15 +1,29 @@
-from prettytable import PrettyTable
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-# table = PrettyTable()
-# table.add_column("Pokemon Name",["Pikachu", "Squirtle", "Charmander"])
-# table.add_column("Type",["Electric", "Water", "Fire"])
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options})")
+
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        money_machine.report()
+        coffee_maker.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
 
 
-#
-table = PrettyTable(["Pokemon Name","Type"])
-table.add_row(["Pikachu", "Electric"])
-table.add_row(["Squirtle", "Water"])
-table.add_row(["Charmander", "Fire"])
 
-print(table)
+
 
---
+++ day_16/try_prettytable.py
@@ -0,0 +1,19 @@
+from prettytable import PrettyTable
+
+# table = PrettyTable()
+# table.add_column("Pokemon Name",["Pikachu", "Squirtle", "Charmander"])
+# table.add_column("Type",["Electric", "Water", "Fire"])
+
+
+#
+table = PrettyTable(["Pokemon Name", "Type"])
+table.add_row(["Pikachu", "Electric"])
+table.add_row(["Squirtle", "Water"])
+table.add_row(["Charmander", "Fire"])
+
+table.align = "l"
+
+print(table.align)
+
+print(table)
+
