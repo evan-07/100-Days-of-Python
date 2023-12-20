596bc1b8e6e5b7d3a9aa107c2a308a2c5cd450aa
van <erivanbuenaventura@gmail.com>
2023-12-20 16:13:26 +0800

    Completed Day 17 exercise using dictionaries, lists, classes and functions

---
+++ day_17/quiz-game-start/quiz_brain.py
@@ -0,0 +1,36 @@
+
+
+
+#attributes
+#question_number = 0
+#question_list = []
+
+#methods
+#next_question()
+
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
+        self.score = 0
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        score, total_q = self.check_answer(user_answer, current_question.answer)
+        return score, total_q
+
+    def still_has_questions(self):
+        return (self.question_number + 1) <= len(self.question_list)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("That's wrong.")
+        print("The correct answer was :", correct_answer)
+        print(f"Your current score is:{self.score}/{self.question_number}")
+        print("\n")
+        return self.score, len(self.question_list)
